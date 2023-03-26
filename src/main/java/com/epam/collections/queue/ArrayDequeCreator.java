d004cd686ec1bfae395f314c26ef9652f8308ff2
d <maksimukart@mail.com>
2023-03-26 11:51:47 +0200

    d

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,28 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        while (!firstQueue.isEmpty()){
+            firstFor(firstQueue, arrayDeque);
+            secondFor(secondQueue, arrayDeque);
+        }
+        return arrayDeque;
+    }
+
+    public void firstFor(Queue<Integer> firstQueue,ArrayDeque<Integer> arrayDeque){
+        if (!arrayDeque.isEmpty())
+            firstQueue.add(arrayDeque.pollLast());
+        for (int i = 0; i < 2; i++)
+            arrayDeque.addLast(firstQueue.poll());
+    }
+
+    public void secondFor(Queue<Integer> secondQueue, ArrayDeque<Integer> arrayDeque){
+        if (arrayDeque.size() != 2)
+            secondQueue.add(arrayDeque.pollLast());
+        for (int i = 0; i < 2; i++)
+            arrayDeque.addLast(secondQueue.poll());
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,24 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> list = new ArrayList<>();
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+        while (!queue.isEmpty()) {
+            for (int i = 1; i < everyDishNumberToEat; i++)
+                queue.addLast(queue.pollFirst());
+            list.add(queue.pollFirst());
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        DishOrderDeterminer dishOrderDeterminer = new DishOrderDeterminer();
+        System.out.println(dishOrderDeterminer.determineDishOrder(10, 3));
     }
 }
